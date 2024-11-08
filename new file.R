7362d265179742dcc2ca15c0ea94930778ebcdb9
fjellmar <mari.fjelldal@gmail.com>
2024-11-08 11:57:12 +0100

    Revert "bug"
    This reverts commit 3fe4c8ab4a0af27063fdeaefe58cf76fa4ce39ed.

--- new file.R
+++ new file.R
@@ -18,7 +18,7 @@ clean_messy_data$species_name <- ifelse(clean_messy_data$species_name == "Lagopa
 x<-c(1,2,3)
 y<-c(2,4,6)
 plot(x,y)
-boxplot(x,Y)
+boxplot(x,y)
 # hello again
 
 # doing some more work
